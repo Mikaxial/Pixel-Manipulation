5cb210f09b2a96f510822a7c9bee51cd75298788
Ftm.amazing <123885130+Mikaxial@users.noreply.github.com>
2024-09-22 17:38:24 +0400

    Pixel Manipulation for Image Encryptio.py

--- Pixel Manipulation for Image Encryptio.py
+++ Pixel Manipulation for Image Encryptio.py
@@ -1,11 +1,10 @@
 from PIL import Image
 import os
 
+#Encryption of the image
 def encrypt_image(image_path, key, output_path):
-    # Open the image
     image = Image.open(image_path)
     encrypted_image = image.copy()
-    # Perform encryption
     for x in range(encrypted_image.width):
         for y in range(encrypted_image.height):
             r, g, b = encrypted_image.getpixel((x, y))
@@ -13,15 +12,13 @@ def encrypt_image(image_path, key, output_path):
             g = (g + key) % 256
             b = (b + key) % 256
             encrypted_image.putpixel((x, y), (r, g, b))
-    # Save the encrypted image
     encrypted_image.save(output_path)
     print(f"Encrypted image saved to {output_path}")
 
+#Decryption of the image
 def decrypt_image(image_path, key, output_path):
-    # Open the encrypted image
     image = Image.open(image_path)
     decrypted_image = image.copy()
-    # Perform decryption
     for x in range(decrypted_image.width):
         for y in range(decrypted_image.height):
             r, g, b = decrypted_image.getpixel((x, y))
@@ -29,7 +26,6 @@ def decrypt_image(image_path, key, output_path):
             g = (g - key) % 256
             b = (b - key) % 256
             decrypted_image.putpixel((x, y), (r, g, b))
-    # Save the decrypted image
     decrypted_image.save(output_path)
     print(f"Decrypted image saved to {output_path}")
 
@@ -38,8 +34,8 @@ def main():
     image_path = input("Enter the path to the image: ")
     key = int(input("Enter the encryption/decryption key (an integer)--> "))
     # File path where the images will be saved
-    encrypted_output_path = "C:/Users/MIS/Downloads/Course/Prodigy Tech/encrypted_image.png"
-    decrypted_output_path = "C:/Users/MIS/Downloads/Course/Prodigy Tech/decrypted_image.png"
+    encrypted_output_path = "C:/Users/MIS/Downloads/Course/Prodigy Tech/Encrypted image/encrypted_image.png"
+    decrypted_output_path = "C:/Users/MIS/Downloads/Course/Prodigy Tech/Decrypted image/decrypted_image.png"
 
     if choice == 'E':
         encrypt_image(image_path, key, encrypted_output_path)
